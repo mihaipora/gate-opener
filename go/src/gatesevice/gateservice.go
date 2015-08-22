9485496421b830cf18b0672f9c6fdddb9bf0d545
Mihai Pora <mihai.pora@gmail.com>
2015-08-22 16:33:03 +0000

    format

--- go/src/gatesevice/gateservice.go
+++ go/src/gatesevice/gateservice.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "log"
-    "os"
-    "os/signal"
-    "time"
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
 
-    "github.com/oleksandr/bonjour"
-    "github.com/stianeikeland/go-rpio"
+	"github.com/oleksandr/bonjour"
+	"github.com/stianeikeland/go-rpio"
 )
 
 const kPort = 9999
@@ -19,23 +19,23 @@ func asyncPushButton(requests chan int, value int) {
 	requests <- value
 }
 
-func buttonHandler(requests chan int) func (http.ResponseWriter, *http.Request) {
+func buttonHandler(requests chan int) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("button handler %v", r)
-                r.ParseForm()
-                button, err := strconv.Atoi(r.Form.Get("k"))
-                log.Printf("form %v button %v", r.Form, button)
-                if err == nil {
-	                go asyncPushButton(requests, button)
+		r.ParseForm()
+		button, err := strconv.Atoi(r.Form.Get("k"))
+		log.Printf("form %v button %v", r.Form, button)
+		if err == nil {
+			go asyncPushButton(requests, button)
 		}
 	}
 }
 
 func processRequests(requests chan int) {
-        pinMap := [...]int{0, 11, 9, 10, 22}
- 	for gate := range requests {
-		log.Printf("processing request %v", gate);
-                if gate < 1 || gate > 4 {
+	pinMap := [...]int{0, 11, 9, 10, 22}
+	for gate := range requests {
+		log.Printf("processing request %v", gate)
+		if gate < 1 || gate > 4 {
 			continue
 		}
 		err := rpio.Open()
@@ -55,30 +55,29 @@ func processRequests(requests chan int) {
 
 func main() {
 
-    // Run registration (blocking call)
-    s, err := bonjour.Register("GateOpen Service", "_gateservice._tcp", "", kPort, []string{"txtv=1", "app=test"}, nil)
-    if err != nil {
-        log.Fatalln(err.Error())
-    }
-    fmt.Println("Service registered")
+	// Run registration (blocking call)
+	s, err := bonjour.Register("GateOpen Service", "_gateservice._tcp", "", kPort, []string{"txtv=1", "app=test"}, nil)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	fmt.Println("Service registered")
 
-    requests := make(chan int)
-    go processRequests(requests)
+	requests := make(chan int)
+	go processRequests(requests)
 
-    http.HandleFunc("/button", buttonHandler(requests))
-    log.Fatal(http.ListenAndServe(":"+strconv.Itoa(kPort) , nil))
+	http.HandleFunc("/button", buttonHandler(requests))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(kPort), nil))
 
-    fmt.Println("Serving")
-    // Ctrl+C handling
-    handler := make(chan os.Signal, 1)
-    signal.Notify(handler, os.Interrupt)
-    for sig := range handler {
-        if sig == os.Interrupt {
-            fmt.Println("Ctrl+C .. quiting")
-            s.Shutdown()
-            time.Sleep(1e9)
-            break
-        }
-    }
+	fmt.Println("Serving")
+	// Ctrl+C handling
+	handler := make(chan os.Signal, 1)
+	signal.Notify(handler, os.Interrupt)
+	for sig := range handler {
+		if sig == os.Interrupt {
+			fmt.Println("Ctrl+C .. quiting")
+			s.Shutdown()
+			time.Sleep(1e9)
+			break
+		}
+	}
 }
-
