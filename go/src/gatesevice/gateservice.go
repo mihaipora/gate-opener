17c6cd06b4a345f2d560b6709009f727008ab01a
Mihai Pora <mihai.pora@gmail.com>
2015-08-22 16:40:40 +0000

    move gate map

--- go/src/gatesevice/gateservice.go
+++ go/src/gatesevice/gateservice.go
@@ -14,6 +14,7 @@ import (
 )
 
 const kPort = 9999
+var kPinMap = [...]int{0, 11, 9, 10, 22}
 
 func asyncPushButton(requests chan int, value int) {
 	requests <- value
@@ -32,10 +33,10 @@ func buttonHandler(requests chan int) func(http.ResponseWriter, *http.Request) {
 }
 
 func processRequests(requests chan int) {
-	pinMap := [...]int{0, 11, 9, 10, 22}
 	for gate := range requests {
 		log.Printf("processing request %v", gate)
-		if gate < 1 || gate > 4 {
+		if gate < 1 || gate >= len(kPinMap) {
+                        log.Printf("Invalid gate number %v", gate)
 			continue
 		}
 		err := rpio.Open()
@@ -43,7 +44,7 @@ func processRequests(requests chan int) {
 			log.Println("Cant open GPIO")
 			continue
 		}
-		pin := rpio.Pin(pinMap[gate])
+		pin := rpio.Pin(kPinMap[gate])
 		pin.Output()
 		pin.High()
 		time.Sleep(time.Second)
