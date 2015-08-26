44ca1da487a6913d9946a1f9176c7578bcc749cc
Mihai Pora <mihai.pora@gmail.com>
2015-08-26 17:15:00 +0000

    update script

--- go/src/gatesevice/gateservice.go
+++ go/src/gatesevice/gateservice.go
@@ -5,14 +5,16 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"encoding/json"
 	"time"
 
-	"github.com/oleksandr/bonjour"
+//	"github.com/oleksandr/bonjour"
 	"github.com/stianeikeland/go-rpio"
 )
 
 const kPort = 9999
-var kPinMap = [...]int{0, 11, 9, 10, 22}
+var kPinMap = [...]int{11, 9, 10, 22}
+var kNameMap = [...]string{"Main Gate", "Main Garage", "Side Gate", "Second Garage"}
 
 func asyncPushButton(requests chan int, value int) {
 	requests <- value
@@ -25,13 +27,16 @@ func buttonHandler(requests chan int) func(http.ResponseWriter, *http.Request) {
 		if err == nil {
 			go asyncPushButton(requests, button)
 		}
+		//TODO: remove sleep
+                time.Sleep(time.Second)
+                w.Write([]byte("Got It!"))
 	}
 }
 
 func processRequests(requests chan int) {
 	for gate := range requests {
 		log.Printf("processing request %v", gate)
-		if gate < 1 || gate >= len(kPinMap) {
+		if gate < 0 || gate >= len(kPinMap) {
                         log.Printf("Invalid gate number %v", gate)
 			continue
 		}
@@ -50,13 +55,22 @@ func processRequests(requests chan int) {
 	}
 }
 
+func config(w http.ResponseWriter, r *http.Request) {
+        msg, err := json.Marshal(kNameMap)
+        if err == nil {
+                w.Write(msg)
+        } else {
+                http.Error(w, "can't read config", 500)
+        }
+}
+
 func main() {
 	// Run registration.
-	_, err := bonjour.Register("GateOpen Service", "_gateservice._tcp", "", kPort, []string{"txtv=1", "app=test"}, nil)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	fmt.Println("Service registered")
+//	_, err := bonjour.Register("gateopenservice", "_gateservice._tcp.", "", kPort, []string{"txtv=1", "app=test"}, nil)
+//	if err != nil {
+//		log.Fatalln(err.Error())
+//	}
+//	fmt.Println("Service registered")
 
 	// Start request processing loop.
 	requests := make(chan int)
@@ -64,5 +78,7 @@ func main() {
 
 	// Handle http requests.
 	http.HandleFunc("/button", buttonHandler(requests))
+	http.HandleFunc("/config", config)
+	fmt.Println("Starting to listen ...")
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(kPort), nil))
 }
